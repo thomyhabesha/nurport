daa3e6a142407e846fcaab67ca82156509f79ab2
thomyhabesha <thomastafese3337@gmail.com>
2023-11-26 22:22:36 +0300

    sxawl

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
 import Home from './page/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}>
+          
+        </Route>
+      </Routes>
+      </BrowserRouter>
      <Home/>
 
     </div>
